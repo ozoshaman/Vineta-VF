2137c7d955661e6f931da64a79e5ee0f3899b2f6
ozoshaman <ozielgomez626@gmail.com>
2025-07-22 23:44:51 -0600

    correccion de config db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,11 @@
-// src/config/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
